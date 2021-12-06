82c976013694b88f19242b56ba19b9d25c5ecac2
François Ehrenmann <francois.ehrenmann@gmail.com>
2021-12-06 16:49:47 +0100

    First add

--- web/config/config.php
+++ web/config/config.php
@@ -1,1 +1,17 @@
+<?php
+#db credentials
+$username =  rawurlencode('userr-pgd');
+$password =  rawurlencode('rrrrr');
 
+#docker mode : 0 = FALSE; 1 = TRUE;
+$docker_mode = 0;
+
+#------------------------
+$database = 'pgd-db';
+$collection = 'metadata';
+
+$uritarget = '127.0.0.1';
+if ($docker_mode==1){$uritarget='biogeco-pgd-db';}
+$uri = "mongodb://$username:$password@$uritarget:27017/$database";
+$client = new MongoDB\Driver\Manager($uri);
+?>
